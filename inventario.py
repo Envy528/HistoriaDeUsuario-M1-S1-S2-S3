980d7d6d4fff16d74bee9d6b89cf2cd88bfe39f2
Envy528 <carmonaballestasj528@gmail.com>
2026-04-02 17:19:32 -0500

    S2 - Task 1: Data validation with conditionals (Menu)

--- inventario.py
+++ inventario.py
@@ -1,22 +1,12 @@
-#Iteration in case the user inputs a wrong value
-while True:
-    #User data validation 
-    try:
-        #User data request 
-        productName = input("Enter the product's name: ")
-        productPrice = float(input("Enter the product's price: "))
-        productQuantity = int(input("Enter the product's quantity: "))
-        break
-    except ValueError:
-        #Error message for wrong user input
-        print("Enter a valid value (E.g.Name = soap; price = 2.3; quantity = 2)")
+opcion = input("1. Add Product\n2. Show Inventory\n3. Calculate statistics\n4. Exit\nYour Option: ")
 
-#Mathematical operation for the total cost of the products
-total_cost = productPrice * productQuantity
-#Message for the user that shows their inputs and the total cost
-print(f"Product: {productName} | Price: {productPrice} | Quantity: {productQuantity} | Total: {total_cost}")
+if opcion == "1":
 
-# The program asks the user for a product name, price and quantity.
-# It does a multiplication between the price and quantity to give
-# the total cost for all of the items and show a brief summary 
-# that contains the name, price, quantity and total cost of the products
+elif opcion == "2":
+
+elif opcion == "3":
+
+elif opcion == "4":
+
+else:
+    print("Invalid input, try again with one of the options (1-4)")
