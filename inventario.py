1527b13d46d9546dea450b152e41566738f9c31b
Envy528 <carmonaballestasj528@gmail.com>
2026-04-02 17:11:15 -0500

    S1 - Task 2: Data Entry

---
+++ inventario.py
@@ -0,0 +1,9 @@
+
+while True:
+    try:
+        productName = input("Enter the product's name: ")
+        productPrice = float(input("Enter the product's price: "))
+        productQuantity = int(input("Enter the product's quantity: "))
+        break
+    except ValueError:
+        print("Enter a valid value (E.g. price = 2.3; quantity = 2)")
